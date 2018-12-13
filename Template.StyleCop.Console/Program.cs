d73b7200aba40b665761c7a536a758791da921b6
Radu B <radu.buciuceanu@gmail.com>
2018-12-13 09:21:31 +0200

    [master] Project structure was made.

---
+++ Template.StyleCop.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Template.StyleCop.Console
+{
+    using System;
+
+    interface IFoo
+    {
+    }
+
+    class B
+    {
+        private string _b;
+
+        public B()
+        {
+            _b = "asd";
+        }
+
+        public string Foo(int a, int b)
+        {
+            return _b;
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (true)
+            {
+                int a = 1 + 1;
+            }
+        }
+    }
+}
